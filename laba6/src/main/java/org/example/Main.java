29104333f907cb9535f61d748a6043769701aed1
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-10-10 11:31:22 +0300

    some commit

--- laba6/src/main/java/org/example/Main.java
+++ laba6/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -8,26 +9,33 @@ public class Main {
     private static final int h = size / 2;
     private static float[] mainArray = new float[size];
 
+    private static final int sizeForTask = 400;
+    private static final int hForTask = sizeForTask / 6;
+    private static int remainder = sizeForTask % 6;
+    private static float[] taskArray = new float[sizeForTask];
     public static void main(String[] args) {
         for (int i = 0; i < size; i++) {
             mainArray[i] = 1;
         }
 
+        for (int i = 0; i < sizeForTask; i++) {
+            taskArray[i] = 1;
+        }
+
         System.out.println("Время выполнения первого метода: " + singleThread(mainArray) + "ms");
         System.out.println("Время выполнения второго метода: " + doubleThread(mainArray) + "ms");
+        System.out.println("Время выполнения третьего метода: " + individualTask(taskArray) + "ms");
+        System.out.println("remainder: " + remainder);
 
-        /*
-        new Thread (()->e1.singleThread(mainArray)).start();
-        new Thread (()->e1.doubleThread(mainArray)).start();
-        */
+
     }
     public static long singleThread(float arr[]){          //один поток
         long start = System.currentTimeMillis();
         for (int i = 0; i < size;i++){
             arr[i]=(float)(arr[i] * Math.sin(0.2f + i/5) * Math.cos(0.2f + i/5) * Math.cos(0.4f + i/2));
         }
-        long total = System.currentTimeMillis() - start;
-        return total;
+        long totalTime = System.currentTimeMillis() - start;
+        return totalTime;
     }
     public static long doubleThread(float arr[]){         //два потока
         float arr1[] = new float[h];
@@ -40,20 +48,77 @@ public class Main {
             for (int i = 0; i < arr1.length;i++){
                 arr1[i]=(float)(arr1[i] * Math.sin(0.2f + i/5) * Math.cos(0.2f + i/5) * Math.cos(0.4f + i/2));
             }
-            //System.out.println("1: "+ Arrays.toString(arr1));
         }).start();
 
         new Thread (()->{
             for (int i = 0; i < arr2.length;i++){
                 arr2[i]=(float)(arr2[i] * Math.sin(0.2f + i/5) * Math.cos(0.2f + i/5) * Math.cos(0.4f + i/2));
+                //System.out.println("Second thread elem: " + arr[i]);
             }
-            //System.out.println("2: "+ Arrays.toString(arr2));
         }).start();
+
         System.arraycopy(arr1, 0, arr, 0, h);
         System.arraycopy(arr2, 0, arr, h, h);
-        long total = System.currentTimeMillis() - start;
-        return total;
+        long totalTime = System.currentTimeMillis() - start;
+        return totalTime;
     }
+
+    public static float[] createThread(float arr[]) {
+        float tempArr[] = new float[hForTask];
+        System.arraycopy(arr, 0, tempArr, 0, hForTask);
+
+        new Thread(() -> {
+            for (int j = 0; j < arr.length;j++){
+                arr[j]=(float)(arr[j] * Math.sin(0.2f + j/5) * Math.cos(0.2f + j/5) * Math.cos(0.4f + j/2));
+            }
+        }).start();
+        return arr;
+    }
+
+    public static long individualTask(float arr[]) {
+        long start = System.currentTimeMillis();
+        float tempArr[] = new float[hForTask];
+        float tempArr2[] = new float[hForTask];
+        float tempArr3[] = new float[hForTask];
+        float tempArr4[] = new float[hForTask];
+        float tempArr5[] = new float[hForTask];
+        float tempArr6[] = new float[hForTask+remainder];
+        ArrayList<float[]> listOfArrs = new ArrayList<>();
+
+        System.arraycopy(arr, 0, tempArr, 0, hForTask);
+        System.arraycopy(arr, hForTask, tempArr2, 0, hForTask);
+        System.arraycopy(arr, hForTask*2, tempArr3, 0, hForTask);
+        System.arraycopy(arr, hForTask*3, tempArr4, 0, hForTask);
+        System.arraycopy(arr, hForTask*4, tempArr5, 0, hForTask);
+        System.arraycopy(arr, hForTask*5, tempArr6, 0, hForTask+remainder);
+
+        listOfArrs.add(tempArr);
+        listOfArrs.add(tempArr2);
+        listOfArrs.add(tempArr3);
+        listOfArrs.add(tempArr4);
+        listOfArrs.add(tempArr5);
+        listOfArrs.add(tempArr6);
+
+        for (int i = 0; i < listOfArrs.size(); i++) {
+            createThread(listOfArrs.get(i));
+        }
+
+        System.arraycopy(tempArr, 0, arr, 0, hForTask);
+        System.arraycopy(tempArr2, 0, arr, hForTask, hForTask);
+        System.arraycopy(tempArr3, 0, arr, hForTask*2, hForTask);
+        System.arraycopy(tempArr4, 0, arr, hForTask*3, hForTask);
+        System.arraycopy(tempArr5, 0, arr, hForTask*4, hForTask);
+        System.arraycopy(tempArr6, 0, arr, hForTask*5, hForTask+remainder);
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(" " + arr[i]);
+        }
+        System.out.println("Значение первой ячейки массива: " + arr[0] + "\nЗначение последней ячейки: " + arr[arr.length-1]);
+
+        long totalTime = System.currentTimeMillis() - start;
+        return totalTime;
+    }
+
 }
 
 
